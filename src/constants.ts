311a1233f8aff0dbae34d5dbc708b2001b85cc64
najafsikander <najafsikander@yahoo.com>
2024-06-28 17:45:08 +0400

    Completed most recent article section

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Tech Perople Blog';
 export const SITE_DESCRIPTION = 'Articles, stories and tutorials from Tech Perople';
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
