b802b2e97386a0e752c4c4601adb8bd678a4f91e
najafsikander <najafsikander@yahoo.com>
2024-07-01 18:47:58 +0400

    completed footer tags feature

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,15 @@
-
+//Format the date to a string
 const formatDate = (date:Date): string => {
     const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long', day: 'numeric'};
   
     return new Date(date).toLocaleDateString(undefined, options);
   }
+  
+  //Capitalize first character of the string
+  const capitalize = (str: string):string => {
+    if(typeof str !== 'string' || str.length === 0) return str;
 
-  export {formatDate};
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  export {formatDate, capitalize};
